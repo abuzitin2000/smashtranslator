0b64fa67f33d17e9b5fe036b41e6f7f5761df570
abuzitin2000 <umutalicoskun@hotmail.com>
2021-07-21 03:58:53 +0300

    Optimized the translation (thanks to u/Altimor) Edited the translation list

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ let translateButton = document.getElementById("smashtranslate");
 // When the button is clicked
 translateButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
+
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: translateSmash,
@@ -17,56 +17,32 @@ function translateSmash() {
   console.log("Translating...");
 
   let translation_data = {
-    "0 to Death": "Touch of Death",
     "0 to death": "touch of death",
-    "Aerial Attack": "Air Normal",
-    "Aerial Attacks": "Air Normals",
     "aerial attack": "air normal",
-    "aerial attacks": "air normals",
-    "Aerial attack": "Air normal",
-    "Aerial attacks": "Air normals",
-    "Air Dodge": "Air Parry",
-    "Air dodge": "Air parry",
-    "air dodge": "air parry",
-    "Chain Grab": "Throw Loop",
-    "Chain grab": "Throw loop",
     "chain grab": "throw loop",
-    "Crew Battle": "Team Battle",
-    "Crew battle": "Team battle",
     "crew battle": "team battle",
-    "Ditto": "Mirror Match",
     "ditto": "mirror match",
-    "Knockback": "Pushback",
     "knockback": "pushback",
-    "Perfect Shield": "Just Block",
-    "Perfect shield": "Just block",
     "perfect shield": "just block",
-    "Powershield": "Just Block",
     "powershield": "just block",
-    "Sex Kick": "Meaty",
-    "Sex kick": "Meaty",
-    "sex kick": "meaty",
-    "Shield": "Block",
+    "sex kick": "air normal with long active frames",
     "shield": "block",
-    "Stock": "Health",
     "stock": "health",
-    "Tilt Attack": "Normal",
-    "Tilt attack": "Normal",
     "tilt attack": "normal",
-    "End Lag": "Recovery",
-    "End lag": "Recovery",
     "end lag": "recovery",
-    "Ending Lag": "Recovery",
-    "Ending lag": "Recovery",
     "ending lag": "recovery",
-    "Start-up Lag": "Start-up Frames",
-    "Start-up lag": "Start-up frames",
     "start-up Lag": "start-up frames",
-    "Startup Lag": "Start-up Frames",
-    "Startup lag": "Start-up frames",
-    "startup Lag": "start-up frames"
+    "startup Lag": "startup frames",
+    "cash battle": "money match",
+    "jump squat": "pre jump",
+    "paid skill": "dlc",
+    "againsies": "runback",
+    "rootie tootie point and shootie": "gun",
+    "mama mia": "wake up dp",
+    "guessie": "mixup",
+    "messy guessie": "really good mixup"
   }
-    
+
   let elements_a = [...document.getElementsByTagName("a")];
 
   let elements_b = [...document.getElementsByTagName("b")];
@@ -80,10 +56,16 @@ function translateSmash() {
   let elements_div = [...document.getElementsByTagName("div")];
 
   let elements = [...elements_a, ...elements_b, ...elements_span, ...elements_p, ...elements_h1, ...elements_div];
-  
+
   elements.forEach(element => {
-    for (let [key, value] of Object.entries(translation_data)) {
-      element.innerHTML = element.innerHTML.replaceAll(key, value);
+    for (let node of element.childNodes) {
+      if (node.nodeType == Node.TEXT_NODE) {
+        for (let [key, value] of Object.entries(translation_data)) {
+          var esc = key.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+          var reg = new RegExp(esc, 'ig');
+          node.data = node.data.replace(reg, value);
+        }
+      }
     }
   });
 
